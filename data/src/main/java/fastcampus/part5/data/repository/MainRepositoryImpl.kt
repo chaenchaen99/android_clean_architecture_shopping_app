7498dfa8a1d5bc1b3791518cd547a886c39f3bb6
chaeyeon <chaenjung.99@gmail.com>
2024-08-15 13:54:53 +0900

    상품정보 기반으로 mock데이터 세팅

---
+++ data/src/main/java/fastcampus/part5/data/repository/MainRepositoryImpl.kt
@@ -0,0 +1,21 @@
+package fastcampus.part5.data.repository
+
+import android.content.Context
+import com.google.gson.GsonBuilder
+import com.google.gson.reflect.TypeToken
+import dagger.hilt.android.qualifiers.ApplicationContext
+import fastcampus.part5.domain.model.Product
+import fastcampus.part5.domain.repository.MainRepository
+
+class MainRepositoryImpl @Inject constructor(
+    @ApplicationContext private val context: Context
+): MainRepository {
+    override fun getProductList(): List<Product> {
+        val inputStream = context.assets.open("product_list.json")
+        val inputStreamReader = InputStreamReader(inputStream)
+        val jsonString = inputStreamReader.readText()
+        val type = object : TypeToken<List<Product>>(){}.type
+
+        return GsonBuilder().create().fromJson(jsonString, type)
+    }
+}
